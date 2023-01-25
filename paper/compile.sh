5be858f71eeae9494ce7cc6c3c91a00d9cef66ac
Robert Redl <robert.redl@lmu.de>
2023-01-25 17:31:56 +0100

    updated paper compile script to use enroot

--- paper/compile.sh
+++ paper/compile.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-docker run --rm \
-    --volume $PWD:/data \
-    --user $(id -u):$(id -g) \
-    --env JOURNAL=joss \
-    openjournals/inara
+# compile the pdf with the openjournals docker image. Use enroot if installed.
+IMAGE=openjournals/inara
+IMAGE_BASENAME=$(dirname $IMAGE)
+if [[ -x "/usr/bin/enroot" ]] ; then
+    echo "using enroot container runtime..."
+    if ! enroot list | grep $IMAGE_BASENAME > /dev/null ; then
+        echo "retrieving container image..."
+        enroot import -o $IMAGE_BASENAME.sqsh docker://$IMAGE
+        enroot create $IMAGE_BASENAME.sqsh
+        rm $IMAGE_BASENAME.sqsh
+    fi
+    export ENROOT_MOUNT_HOME=y
+    enroot start --mount $PWD:/data --env JOURNAL=joss $(dirname $IMAGE)
+else
+    echo "using docker container runtime..."
+    docker run --rm \
+        --volume $PWD:/data \
+        --user $(id -u):$(id -g) \
+        --env JOURNAL=joss \
+        $IMAGE
+fi
