fc833e8c8d6e7df4a214ec0ff2984931856cce80
Yvonne Ruckstuhl <yvonne.ruckstuhl@physik.uni-muenchen.de>
2024-02-27 11:46:29 +0100

    bla

--- enstools/da/freda/algorithms/QPEns.py
+++ enstools/da/freda/algorithms/QPEns.py
@@ -19,13 +19,11 @@ class QPEns(Algorithm):
         """
         # temporal variables
         det = 0
-        print("################11111111111111111111111111111111111############################")
         n_varlayer = state.shape[1]
         n_ens = state.shape[2]
         n_inv = 1. / (n_ens - 1)
         equivalent = np.empty(n_ens, dtype=np.float32)
         deviation_equivalent_mean = np.empty(n_ens, dtype=np.float32)
-        print("####################222222222222222222222222222222###############################")
         # observations are processed one by one in the order that they are listed in the reports array
         for i_report in range(reports.shape[0]):
             # all observations in this report are located at this index within the local part of the grid.
@@ -33,41 +31,26 @@ class QPEns(Algorithm):
             innovation = np.empty((reports[i_report, 1],n_ens), dtype=np.float32)
             Y = np.empty((reports[i_report, 1],n_ens), dtype=np.float32)
             R = np.empty(reports[i_report, 1], dtype=np.float32)
-            print(i_report,"#####################333333333333333333333333#######################")
             assert grid_index != -1
             
             # loop over all observations in this report
             for i_obs in range(reports[i_report, 0], reports[i_report, 0] + reports[i_report, 1]):
-                print(i_report,i_obs,"#####################looooooooooooooooooooooop######################")
                 obs_layer = int(observations[i_obs,2])
                 mlevel_obs = state_map_inverse[obs_layer,1]
                 # get model equivalents for the given observation and the mean which is later used for covariances
                 # for observation on model levels, model_equivalent returns just the corresponding gird cell.
                 equivalent_mean = model_equivalent(state, state_map, grid_index, observations, observation_type, i_obs,
                                  equivalent, deviation_equivalent_mean)
-                print(i_report,i_obs,"################################4444444444444444444444444444444444#################################")
+                
                 # calculate innovation from observation value[i_obs, 0] and observation error[i_obs, 0]
-               #
-                #print(observations[i_obs, 0].shape)
-                #print(np.random.normal(0, observations[i_obs, 1], n_ens-det).shape,equivalent.shape,innovation[i_obs-reports[i_report, 0],:].shape)
-                print(np.random.normal(0, observations[i_obs, 1], n_ens-det).shape)
-                print(equivalent.shape)
-                print(innovation[i_obs-reports[i_report, 0],:].shape)
                 innovation[i_obs-reports[i_report, 0],:] = observations[i_obs, 0] + np.random.normal(0, observations[i_obs, 1], n_ens-det) - equivalent
-                print(i_report,i_obs,"####################555555555555555555555555555555555##################")
                 Y[i_obs-reports[i_report, 0],:] = deviation_equivalent_mean 
-                print(i_report,i_obs,"#########################66666666666666666666666666666#########################")
                 R[i_obs-reports[i_report, 0]] = observations[i_obs, 1]**2 
-                print(i_report,i_obs,"#############################7777777777777777777777777#############################")
             
             # Solve Av=b
-            print("#############SOLVING THE SYSTEM#####################")
             A = n_inv * rho[mlevel_obs] * np.dot(Y,Y.T) + np.diag(R)
-            #assert innovation.dtype == np.dtype(np.float32)
             v = np.empty((reports[i_report, 1], n_ens), dtype=np.float32)
             v[:] = lin_solve(A,innovation)
-            print("#############SYSTEM SOLVED###########################")
-            #v = innovation #np.linalg.solve(A,innovation)
             # loop over all grid cells and all variables that are within the localization radius
             # This loop runs in parallel if NUMBA_NUM_THREADS is larger than 1.
             i_points = reports[i_report, 3]
